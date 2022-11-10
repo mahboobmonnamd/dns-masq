70529649a95f713b3561762caf03022843827aa5
Mahboob <mahboobmonnamd@gmail.com>
2022-11-10 11:17:55 +0530

    refactor: formatting

--- src/docker/util.ts
+++ src/docker/util.ts
@@ -2,12 +2,12 @@ import Docker from 'dockerode';
 
 const docker = Docker();
 
-export const getImage = async tag => {
+export const getImage = async (tag) => {
   const images = await docker.listImages({ filters: { reference: [tag] } });
   return images[0];
 };
 
-export const getContainer = async name => {
+export const getContainer = async (name) => {
   const containers = await docker.listContainers({ all: true, filters: { name: [name] } });
   return containers[0];
 };
